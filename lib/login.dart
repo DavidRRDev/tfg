2d7d997264989f05545b6332cc29406baf5a1365
David Rojas Rodríguez <davidrr.dev@gmail.com>
2024-04-21 15:32:51 +0200

    pagina de inicio hacer segunda pagina

---
+++ lib/login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Container(
+        child: Text('Introduce  tu usuario  y contraseña'),
+        
+      ),
+    );
+  }
+}
