7dc3f64008d85e767752cd95e259e39407b32351
David Rojas Rodríguez <davidrr.dev@gmail.com>
2024-05-16 18:46:49 +0200

    base de datos conectado

--- lib/registro.dart
+++ lib/registro.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Agregado para autenticación
 
-void main() {
+import 'login.dart'; // Suponiendo que tienes una pantalla de inicio de sesión separada
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -17,12 +24,87 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Registro extends StatelessWidget {
+class Registro extends StatefulWidget {
+  @override
+  _RegistroState createState() => _RegistroState();
+}
+
+class _RegistroState extends State<Registro> {
+  final TextEditingController _nombreController = TextEditingController();
+  final TextEditingController _correoController = TextEditingController();
+  final TextEditingController _contrasenaController = TextEditingController();
+  final TextEditingController _confirmarContrasenaController =
+  TextEditingController();
+
+  Future<void> _registerUserToFirebase(BuildContext context) async {
+    try {
+      // Validar si las contraseñas coinciden
+      if (_contrasenaController.text != _confirmarContrasenaController.text) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Error de registro'),
+              content: Text('Las contraseñas no coinciden.'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Aceptar'),
+                ),
+              ],
+            );
+          },
+        );
+        return;
+      }
+
+      // Registrar el usuario en Firebase Firestore
+      await FirebaseFirestore.instance.collection('usuarios').add({
+        'nombre': _nombreController.text,
+        'correo': _correoController.text,
+        // Agrega más campos según sea necesario
+      });
+
+      // Registrar el usuario en Firebase Authentication
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _correoController.text,
+        password: _contrasenaController.text,
+      );
+
+      // Redirigir al usuario al menú principal (en este caso, al inicio de sesión)
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Login()),
+      );
+    } catch (e) {
+      print('Error al registrar usuario: $e');
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error de registro'),
+            content: Text(
+                'Ocurrió un error al registrar al usuario. Por favor, inténtalo de nuevo más tarde.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Aceptar'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset:
-          false, // Evitar desbordamiento de píxeles cuando el teclado está presente
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -61,6 +143,7 @@ class Registro extends StatelessWidget {
                     child: Column(
                       children: [
                         TextFormField(
+                          controller: _nombreController,
                           style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             labelText: 'Nombre',
@@ -73,6 +156,7 @@ class Registro extends StatelessWidget {
                           height: 1,
                         ),
                         TextFormField(
+                          controller: _correoController,
                           style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             labelText: 'Correo Electrónico',
@@ -85,6 +169,7 @@ class Registro extends StatelessWidget {
                           height: 1,
                         ),
                         TextFormField(
+                          controller: _contrasenaController,
                           style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             labelText: 'Contraseña',
@@ -98,6 +183,7 @@ class Registro extends StatelessWidget {
                           height: 1,
                         ),
                         TextFormField(
+                          controller: _confirmarContrasenaController,
                           style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
                             labelText: 'Confirmar Contraseña',
@@ -113,7 +199,7 @@ class Registro extends StatelessWidget {
                 SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    //Pendiente de agregar la lógica
+                    _registerUserToFirebase(context);
                   },
                   child: Text('Enviar'),
                 ),
